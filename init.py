e6b3e55a48cd7e5a29e7c40444658a68d1241357
40823245 <40823245@gm.nfu.edu.tw>
2019-11-14 16:37:24 +0800

    initial edit web

--- init.py
+++ init.py
@@ -10,7 +10,7 @@ config_dir = _curdir + "/config/"
 class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
-    site_title = "cp2019ag1"
+    site_title = "cp2019bg4"
     def __init__(self):
         # hope to create downloads and images directories　
         if not os.path.isdir(_curdir + "/downloads"):
